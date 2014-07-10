f822ac714ff488118e85a073fcf0ae3f975ff728
Ben Blamey <blamey.ben@gmail.com>
2014-07-10 10:03:22 +0100

    .

--- copy_to_thesis.bat
+++ copy_to_thesis.bat
@@ -1,3 +1,3 @@
-xcopy *.pdf C:\work\docs\phd_work\thesis\images\
-xcopy table_*.tex C:\work\docs\phd_work\thesis\images\
+xcopy /y output\*.pdf C:\work\docs\phd_work\thesis\images\
+xcopy /y output\*.tex C:\work\docs\phd_work\thesis\images\
 pause
